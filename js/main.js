a8e34586118e0d54de7ac1621985dde836b42200
Imelda Alonso <adlewic@gmail.com>
2016-07-28 15:35:14 -0400

    fix the display

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 // recovering the that audio on the html file 
 var audioElement = document.getElementsByTagName('audio')[0]
+
+
 // varibles for buttons Play and Pause , we will use this for the same button
 var play_button = document.getElementById('play_icon');
 var pause_button = document.getElementById('pause_icon');
-
-
-
 
 
 // this is the  contructor --Jukebox--
@@ -28,11 +27,12 @@ Jukebox.prototype.setSong = function() {
 
 
 // function play 
-
 Jukebox.prototype.play = function() {
   audioElement.play()
 
-  // you can change  the icon of the  play/pause button
+// this displays the name of the current song 
+  this.showname()
+// you can change  the icon of the  play/pause button
   play_button.style.display = "none"
   pause_button.style.display = "inline-block"
 }
@@ -48,7 +48,6 @@ Jukebox.prototype.pause = function() {
 }
 
 
-
 Jukebox.prototype.stop = function() {
   this.pause()
   audioElement.currentTime = 0
@@ -56,7 +55,8 @@ Jukebox.prototype.stop = function() {
 
 Jukebox.prototype.next = function() {
   this.i++
-  // stops audio if at end of playlist
+  this.showname()
+
   if (this.i == this.songs.length) {
     this.i--
     this.stop()
@@ -64,11 +64,13 @@ Jukebox.prototype.next = function() {
     this.setSong()
     this.play()
   }
+
 }
+
 
 Jukebox.prototype.previous = function() {
   this.i--
-  // stops audio if at start of playlist
+
   if (this.i < 0) {
     this.i++
     this.stop()
@@ -79,10 +81,34 @@ Jukebox.prototype.previous = function() {
 }
 
 
+// this function is to display de name of the song playing 
+Jukebox.prototype.showname = function(){
+
+    // alert(this.i)
+    if (this.songs[this.i].title == "Litoral"){
+      document.getElementById('title').innerHTML= this.songs[0].title;
+      document.getElementById('artist').innerHTML= this.songs[0].artist;
+
+    } else if(this.songs[this.i].title == "Lights") {
+      document.getElementById('title').innerHTML= this.songs[1].title;
+      document.getElementById('artist').innerHTML= this.songs[1].artist;
+
+    } else if(this.songs[this.i].title == "And Wot?") {
+      document.getElementById('title').innerHTML= this.songs[2].title;
+      document.getElementById('artist').innerHTML= this.songs[2].artist
+
+    } else if(this.songs[this.i].title =="Millionare"){
+      document.getElementById('title').innerHTML= this.songs[3].title;
+      document.getElementById('artist').innerHTML= this.songs[3].artist;
+
+    }
+
+}
 
 
-// this is to load the songs 
 
+
+// this is to push the songs in the array
 Jukebox.prototype.loadSongs = function() {
   for (var i = 0; i < arguments.length; i++) { 
     this.songs.push(arguments[i])    
@@ -100,31 +126,29 @@ Jukebox.prototype.listSongs = function() {
 
 
 // constructor for the object Song 
-
 function Song(artist, title, url) {
   this.artist = artist
   this.title = title
   this.url = url
+  this.isPlaying = false;
 }
-
-// this is just a easy way to storage owr jukebox 
-var Myjukebox = new Jukebox()
-
-// this are the new songs 
-var song1 = new Song("Mitú", "Litoral", "audio/1_one.mp3")
-var song2 = new Song("Sohn", "Lights", "audio/2_two.mp3")
-var song3 = new Song("And Wot?", "TroyBoi", "audio/3_three.mp3")
-
-
-
-Myjukebox.loadSongs(song1,song2,song3)
-
 
 
 window.onload = function() {
   Myjukebox.initialize()
 }
 
+
+// this is just a easy way to storage our jukebox 
+var Myjukebox = new Jukebox();
+
+// this are the new songs 
+var song1 = new Song("Mitú", "Litoral", "audio/1_one.mp3")               // 0
+var song2 = new Song("Sohn", "Lights", "audio/2_two.mp3")                // 1
+var song3 = new Song("TroyBoi","And Wot?","audio/3_three.mp3")           // 2
+var song4 = new Song("Plastilina Mosh","Millionare","audio/4_four.mp3")  // 3
+
+Myjukebox.loadSongs(song1,song2,song3,song4)
 
 
 // this are the Buttons with actions 
@@ -137,14 +161,12 @@ document.getElementById("pause_icon").addEventListener("click", function() {
 })
 
 document.getElementById("next_icon").addEventListener("click", function() {
- 
   Myjukebox.next()
 })
 
 document.getElementById("previous_icon").addEventListener("click", function() {
   Myjukebox.previous()
 })
-
 
 // auto advance to the next song at end of song
 audioElement.addEventListener("ended", function() {
